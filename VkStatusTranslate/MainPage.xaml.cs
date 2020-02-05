81c93f14fc7f570be1892df4a9cd9ac36d178761
OOjeser <50872233+OOjeser@users.noreply.github.com>
2020-02-05 22:19:26 +0500

    довел до кое как рабочего состояния

--- VkStatusTranslate/MainPage.xaml.cs
+++ VkStatusTranslate/MainPage.xaml.cs
@@ -16,6 +16,9 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using System.Diagnostics;
+using Windows.System.Diagnostics;
+using Windows.UI;
+using Windows.System;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
 
@@ -31,27 +34,41 @@ namespace VkStatusTranslate
             this.InitializeComponent();
         }
 
-        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        private async void Grid_Loaded(object sender, RoutedEventArgs e)
         {
+            DiagnosticAccessStatus diagnosticAccessStatus = await AppDiagnosticInfo.RequestAccessAsync();
+            Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
             try
             {
-                foreach (var i in (Dictionary<string, string>)singlton.localSettings.Values["Proc"]) 
+                var s = "";
+                Windows.Storage.StorageFile sampleFile = await storageFolder.GetFileAsync("procs.txt");
+                foreach (var i in (await Windows.Storage.FileIO.ReadTextAsync(sampleFile)).Split('\n')) 
                 {
-                    foreach (var ii in Process.GetProcesses()) 
+                    foreach (ProcessDiagnosticInfo ii in ProcessDiagnosticInfo.GetForProcesses()) 
                     {
-                        if (i.Key == ii.ProcessName) 
+                        s += " " + ii.ExecutableFileName;
+                        if (i.Split('~')[0] == ii.ExecutableFileName) 
                         {
-                            singlton.status = i.Value;
+                            singlton.status = i.Split('~')[1];
                         }
                     }
+                    Grid g = new Grid() { HorizontalAlignment = HorizontalAlignment.Stretch, VerticalAlignment = VerticalAlignment.Stretch };
+                    ListViewItem lvi = new ListViewItem() { Content = g, HorizontalAlignment = HorizontalAlignment.Stretch, HorizontalContentAlignment = HorizontalAlignment.Stretch };
+                    g.Children.Add(new TextBlock() { Text = i.Split('~')[0], HorizontalAlignment = HorizontalAlignment.Left });
+                    g.Children.Add(new TextBlock() { Text = i.Split('~')[1], HorizontalAlignment = HorizontalAlignment.Right });
+                    list.Items.Add(lvi);
                 }
+                answTextBlock.Text = singlton.status;
+                //await Windows.Storage.FileIO.WriteTextAsync(sampleFile, "Test~System\nexplorer.exe~в проводнике");
             }
             catch (Exception ex) 
             {
-                singlton.localSettings.Values["Proc"] = new Dictionary<string, string>();
+                Windows.Storage.StorageFile sampleFile = await storageFolder.CreateFileAsync("procs.txt", Windows.Storage.CreationCollisionOption.ReplaceExisting);
+                answTextBlock.Text = ex.Message;
+                await Windows.Storage.FileIO.WriteTextAsync(sampleFile, "Test~System");
             }
 
-            VkApi api = new VkApi();
+            /*VkApi api = new VkApi();
             api.Authorize(new ApiAuthParams()
             {
                 Login = singlton.login,
@@ -60,7 +77,7 @@ namespace VkStatusTranslate
                 Settings = Settings.All
             });
             var respone = api.Status.Set("test1");
-            answTextBlock.Text = respone.ToString();
+            answTextBlock.Text = respone.ToString();*/
         }
     }
 }
