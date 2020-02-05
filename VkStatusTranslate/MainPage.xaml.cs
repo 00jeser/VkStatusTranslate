d834181bd34c7cb7435d47e5297dfebd9c474b4d
OOjeser <50872233+OOjeser@users.noreply.github.com>
2020-02-05 21:26:10 +0500

    начало реализации

--- VkStatusTranslate/MainPage.xaml.cs
+++ VkStatusTranslate/MainPage.xaml.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using VkNet;
+using VkNet.Model;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
+using VkNet.Enums.Filters;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
 
@@ -25,6 +29,38 @@ namespace VkStatusTranslate
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                foreach (var i in (Dictionary<string, string>)singlton.localSettings.Values["Proc"]) 
+                {
+                    foreach (var ii in Process.GetProcesses()) 
+                    {
+                        if (i.Key == ii.ProcessName) 
+                        {
+                            singlton.status = i.Value;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) 
+            {
+                singlton.localSettings.Values["Proc"] = new Dictionary<string, string>();
+            }
+
+            VkApi api = new VkApi();
+            api.Authorize(new ApiAuthParams()
+            {
+                Login = singlton.login,
+                Password = singlton.password,
+                ApplicationId = singlton.appId,
+                Settings = Settings.All
+            });
+            var respone = api.Status.Set("test1");
+            answTextBlock.Text = respone.ToString();
         }
     }
 }
---
+++ VkStatusTranslate/singlton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace VkStatusTranslate
+{
+    static public class singlton
+    {
+        public static string login = "";
+        public static string password = "";
+        public static ulong appId = 1;
+        public static ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+        public static string status = "";
+    }
+}
